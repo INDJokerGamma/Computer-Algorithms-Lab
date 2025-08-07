79acd330780c0ba08eca3d02ba0c5a0d9681b605
INDJokerGamma <piyushdawkhare0000@gmail.com>
2025-08-07 11:38:00 +0530

    adding files and Assignment 2 Q 3

---
+++ Assignment_2/Q3.cpp
@@ -0,0 +1,31 @@
+ 
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+void TowerofHanoi(int n, char s_rod, char d_rod, char a_rod )
+{
+    if (n==1)
+    {
+        cout << "Moving Disk 1 from rod "<< s_rod << " to rod " << d_rod <<endl;
+        return;
+    }
+    
+    TowerofHanoi (n-1, s_rod, a_rod, d_rod);
+
+    cout<<"Move disk "<< n << " from rod " << s_rod << " to "  << d_rod <<endl;
+    cout<<endl;
+
+    TowerofHanoi (n-1, a_rod, d_rod, s_rod);
+}
+int main () 
+{
+   int n;
+   cout<< "Enter the number of disks"<<endl;
+   cin>>n; 
+   cout<< "The order of the rods are like: "<<endl;
+   cout<<endl;
+   TowerofHanoi (n, 'A', 'B', 'C');
+   
+return 0 ;
+}
